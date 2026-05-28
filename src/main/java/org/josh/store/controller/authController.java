5ca410b844db3a2900040ec140678c526fc6ae8a
I-Josh-pro-grammin <izerejoshua94@gmail.com>
2026-05-28 11:32:22 +0200

    added authorization

--- src/main/java/org/josh/store/controller/authController.java
+++ src/main/java/org/josh/store/controller/authController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class authController {
     @Autowired
     UserService userService;
+
+    @Autowired
     JwtService jwtService;
 
     @PostMapping("register")
